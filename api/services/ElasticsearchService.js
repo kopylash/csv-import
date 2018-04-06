5777ac20fece6f4218ce58249facc5ced976c9fc
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2018-04-06 18:32:47 +0300

    implemented elasticsearch methods: index creation and deletion, bulk insertion of data

--- api/services/ElasticsearchService.js
+++ api/services/ElasticsearchService.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const log = require('../../server/logger');
 const client = require('../../server/elasticConnector')();
 
 module.exports = {
@@ -9,6 +8,27 @@ module.exports = {
   },
 
   bulkInsert(bulkPayload) {
+    return client.bulk({body: bulkPayload});
+  },
 
+  createIndex(index) {
+    return client.indices.create({
+      index,
+      body: {
+        settings: {
+          index: {
+            'number_of_shards': 3,
+            'number_of_replicas': 0
+          }
+        }
+      }
+    });
+  },
+
+  deleteIndex(index) {
+    return client.indices.delete({
+      index,
+      ignoreUnavailable: true
+    });
   }
 };
