d50047bc24722eb0c99cddc728ddd4f9b5772477
Vladyslav Kopylash <vladik.kopilash@gmail.com>
2018-04-10 12:09:03 +0300

    added error handling to jobs status checking

--- app/import/actions.js
+++ app/import/actions.js
@@ -3,5 +3,11 @@
 import CONSTANTS from '../constants';
 
 export const checkJobsStatus = () => {
-  return fetch(`${CONSTANTS.apiURL}/jobs`).then(res => res.json());
+  return fetch(`${CONSTANTS.apiURL}/jobs`)
+    .then(res => res.json())
+    .catch(error => {
+      console.error(error);
+
+      return [];
+    });
 };
